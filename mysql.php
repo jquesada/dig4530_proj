f00ebfac9ac604afc90cdd07be2fe5fcd4704144
SVAD Student <svadstudent@SVAD-07-134-06.local>
2012-10-26 13:52:55 -0400

    remove old mysql file

--- mysql.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$db=mysql_connect("sulley.cah.ucf.edu", "ju773928", "knights123!");
-mysql_select_db("ju773928", $db);
-$db = mysql_select_db("ju773928") or print "connect failed because ".mysql_error();
-
-?>
