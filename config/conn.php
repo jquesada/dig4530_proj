5d9253d5698af13c1d53c58632e75e46abb66312
Sarah <sarah@cloudspace.com>
2012-11-08 20:40:51 -0500

    update connection info

--- config/conn.php
+++ config/conn.php
@@ -1,9 +1,18 @@
 <?php
+
+//if you're using MAMP/WAMP, make the following changes:
+//$host = "localhost"
+//$dbname = <whatever you named the db>
+//$username = "root"
+//$password = <whatever your root password is
+
 try 
 {
-    $username = "root";
-    $password = "root";    
-    $conn = new PDO("mysql:host=localhost;dbname=oojo", $username, $password);
+	$host = "sulley.cah.ucf.edu";
+	$dbname = "ju773928"
+    $username = "ju773928";
+    $password = "knights123!";    
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
 } 
 catch (PDOException $e) 
